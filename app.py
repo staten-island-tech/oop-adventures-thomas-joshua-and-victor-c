324d6fa51310ebb44ee0d2e845864ce97977e1b3
Joshua Zaytsev <joshuaz49@nycstudents.net>
2024-12-06 10:17:16 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+class Player:
+    def __init__(self, name, HP, attack):
+        self.name = name
+        self.HP = HP
+        self.attack = attack
+
+J = Player("J", 100, 2)
+
+class Enemy:
+    def __init__(self, name, HP, attack):
+        self.name = name
+        self.HP = HP
+        self.attack = attack
+
