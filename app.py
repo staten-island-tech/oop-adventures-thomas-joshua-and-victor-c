a55e83cb783090eecf08f0bd6bbc5a62e70f119a
Joshua Zaytsev <joshuaz49@nycstudents.net>
2024-12-12 09:51:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import random
 
 class Player:
-    def __init__(self, name, HP, attack):
+    def __init__(self, name, HP, attack, money):
         self.name = name
         self.HP = HP
         self.attack = attack
+        self.money = money
 
     def PlayerPunch(self, enemy):
         damage = random.randint(1, self.attack) 
@@ -17,10 +18,11 @@ class Player:
         print(f"{self.name} is guarding!")
 
 class Enemy:
-    def __init__(self, name, HP, attack):
+    def __init__(self, name, HP, attack, drop):
         self.name = name
         self.HP = HP
         self.attack = attack
+        self.drop = drop
 
     def EnemyAI(self):
         return random.randint(1, 2)
@@ -31,12 +33,15 @@ class Enemy:
         print(f"{self.name} attacks {player.name} for {damage} damage!")
         if player.HP <= 0:
             print(f"{player.name} has been defeated!")
-
     def EGuard(self):
         print(f"{self.name} is guarding!")
+
+    def EnemyDrop(self):
+        print(f"{J.name} has gained {self.drop} dollars!")
+        return self.drop
 # Example setup: Player vs Enemy
-J = Player("MCHammer", 100, 10)
-enemy = Enemy("Bub", 50, 10)
+J = Player("MCHammer", 100, 10, 0)
+enemy = Enemy("Bub", 50, 10, 20)
 
 PlayerGuard = False
 EnemyGuard = False
@@ -45,10 +50,10 @@ while J.HP > 0 and enemy.HP > 0:
     control = input("Move? ").lower()
     enemy.EnemyAI()
     # Player attacks first
-    if control == "punch":
+    if control == "punch" or control == "p" or control == "1":
         J.PlayerPunch(enemy)
         PlayerGuard = False
-    elif control == "guard":
+    elif control == "guard" or control == "g" or control == "2":
         J.PGuard()
         PlayerGuard = True
         print(f"{enemy.name} could not attack!")
@@ -60,13 +65,15 @@ while J.HP > 0 and enemy.HP > 0:
             enemy.EnemyPunch(J)
     if EnemySelect == 2:
         enemy.EGuard()
-        EnemyGuard = True
         print(f"{J.name} could not attack!")  
     if J.HP <= 0:
         break  
 
 if J.HP > 0:
     print(f"{J.name} wins the battle!")
+    cash_gain = enemy.EnemyDrop()
+    J.money += cash_gain
+    print(J)
 else:
     print(f"{enemy.name} wins the battle!")
 
